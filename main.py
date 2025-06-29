475a0883cbcb0a9d38fca9e94477f432e848a551
Max <maxmcgillwork@gmail.com>
2025-06-29 18:01:42 +0100

    basic bot working

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from typing import Final 
 import os 
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from responses import get_response
+#from responses import get_response
 
 load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class MyClient(Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
